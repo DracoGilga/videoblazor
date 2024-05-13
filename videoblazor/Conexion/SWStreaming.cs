79012cfb5ba9133d5a91357b711c3a1bcc95e1a1
DracoGilga <cesar96707@gmail.com>
2024-05-12 23:51:19 -0600

    Agregar archivos de proyecto.

---
+++ videoblazor/Conexion/SWStreaming.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System.IO;
+using System.Threading.Tasks;
+using static VideoService;
+
+namespace videoblazor.Conexion
+{
+    public class SWStreaming
+    {
+        private readonly VideoServiceClient _client;
+
+        public SWStreaming()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:8080"); // Cambia la dirección según tu servidor gRPC
+            _client = new VideoServiceClient(channel);
+        }
+
+        public async Task<Stream> GetVideoStreamAsync(string videoName)
+        {
+            // Llama al método gRPC para obtener el stream del video
+            var call = _client.downloadVideo(new DownloadFileRequest { Nombre = videoName });
+
+            // Crea un MemoryStream para almacenar los datos del video
+            var videoStream = new MemoryStream();
+
+            // Lee cada fragmento de datos y lo escribe en el MemoryStream
+            await foreach (var chunk in call.ResponseStream.ReadAllAsync())
+                await videoStream.WriteAsync(chunk.Data.ToByteArray(), 0, chunk.Data.Length);
+            
+            videoStream.Position = 0;
+
+            return videoStream;
+        }
+    }
+}
