5b75df350c91c418750e186b01dbaafa191fbb96
DracoGilga <cesar96707@gmail.com>
2024-05-13 00:57:14 -0600

    funtional mode

--- videoblazor/Conexion/SWStreaming.cs
+++ videoblazor/Conexion/SWStreaming.cs
@@ -12,11 +12,12 @@ namespace videoblazor.Conexion
 
         public SWStreaming()
         {
-            var channel = GrpcChannel.ForAddress("http://localhost:8080"); // Cambia la dirección según tu servidor gRPC
+            // url del canal grpc
+            var channel = GrpcChannel.ForAddress("http://localhost:8080"); 
             _client = new VideoServiceClient(channel);
         }
 
-        public async Task<Stream> GetVideoStreamAsync(string videoName)
+        public async Task<MemoryStream> GetVideoStreamAsync(string videoName)
         {
             // Llama al método gRPC para obtener el stream del video
             var call = _client.downloadVideo(new DownloadFileRequest { Nombre = videoName });
@@ -27,7 +28,7 @@ namespace videoblazor.Conexion
             // Lee cada fragmento de datos y lo escribe en el MemoryStream
             await foreach (var chunk in call.ResponseStream.ReadAllAsync())
                 await videoStream.WriteAsync(chunk.Data.ToByteArray(), 0, chunk.Data.Length);
-            
+
             videoStream.Position = 0;
 
             return videoStream;
